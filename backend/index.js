8456b14d1d8f9f2b392f779bbf00de0c545e1ea2
AyushRai7 <ayushrai1729@gmail.com>
2024-07-21 13:19:46 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,25 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser"); // Corrected typo
+const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 require("./Models/db");
 const authRouter = require("./Routes/authrouter");
-const productrouter=require('./Routes/productrouter')
+const productRouter = require("./Routes/productrouter");
 const PORT = process.env.PORT;
-const path=require("path");
+const path = require("path");
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/auth", authRouter);
-app.use("/products", productrouter);
+app.use("/products", productRouter);
 
-app.get("/", (req,res)=>{
-  app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
+app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
+
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
